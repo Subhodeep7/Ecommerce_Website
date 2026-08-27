8cbd2252ac365c9f84172401b6ae551c736cc02d
Subhodeep Mitra <mitrasubhodeep248@gmail.com>
2026-08-27 14:30:01 +0530

    Entity classes created

--- src/main/java/com/project5/Ecommz/model/Orders.java
+++ src/main/java/com/project5/Ecommz/model/Orders.java
@@ -3,6 +3,8 @@ package com.project5.Ecommz.model;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import java.util.Date;
+import java.util.List;
+
 @Entity
 public class Orders {
 
@@ -18,7 +20,58 @@ public class Orders {
 
     private String status;
 
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
     private double totalAmount;
 
     private Date orderDate;
+
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
 }
